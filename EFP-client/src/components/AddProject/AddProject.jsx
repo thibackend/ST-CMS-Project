bb3d09396ad94f888c6852112dd8373ed12b475c
HoVanDi <hovandideveloper@gmail.com>
2024-01-02 22:25:07 +0700

    Update code ui edit/add project page

--- EFP-client/src/components/AddProject/AddProject.jsx
+++ EFP-client/src/components/AddProject/AddProject.jsx
@@ -8,43 +8,45 @@ const { Option } = Select;
 const App = () => {
   return (
     <div className="container">
-      <h2 className="tile"> Add Project</h2>
-      <Form
-        name="basic"
-        initialValues={{
-          remember: true,
-        }}
-        autoComplete="off"
-      >
-        <div className="groupform">
-          <div className="form">
-            <Form.Item>
-              <Input placeholder="Name" />
-              <div className="date">
-                <DatePicker placeholder="Project Start Date" />
-                <DatePicker placeholder="Project End Date" />
-              </div>
+      <div className="container-form">
+        <h2 className="tile"> Add Project</h2>
+        <Form
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          autoComplete="off"
+        >
+          <div className="groupform">
+            <div className="form">
+              <Form.Item>
+                <Input placeholder="Name" />
+                <div className="date">
+                  <DatePicker placeholder="Project Start Date" />
+                  <DatePicker placeholder="Project End Date" />
+                </div>
 
-              <Select placeholder="Technology">
-                <Option value="react">React</Option>
-                <Option value="angular">Angular</Option>
-                <Option value="vue">Vue</Option>
-              </Select>
-            </Form.Item>
+                <Select placeholder="Technology">
+                  <Option value="react">React</Option>
+                  <Option value="angular">Angular</Option>
+                  <Option value="vue">Vue</Option>
+                </Select>
+              </Form.Item>
+            </div>
+
+            <div className="form">
+              <Form.Item>
+                <Input placeholder="Project Name" />
+                <TextArea rows={5} placeholder="Description" maxLength={100} />
+              </Form.Item>
+            </div>
           </div>
 
-          <div className="form">
-            <Form.Item>
-              <Input placeholder="Project Name" />
-              <TextArea rows={5} placeholder="Description" maxLength={100} />
-            </Form.Item>
-          </div>
-        </div>
-
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
