197816f2bbfedda711ea9ec873b28da4eab0e2fc
HuongPhanDev <HuongPhanDev>
2024-01-02 23:09:55 +0700

    refactor: create main layout and config route

---
+++ EFP-client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+function Dashboard() {
+  return (
+    <>
+       <div style={{color:"black", zIndex:99}}>Hi</div>
+    </>
+  )
+}
+
+export default Dashboard
