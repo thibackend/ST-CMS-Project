c594ed96e15d59f551b6227fccdb4c40bad19646
Tuyen Error <caotuyenks1605@gmail.com>
2024-01-03 10:37:14 +0700

    interface:Create Dashboard with Chart and Cards

--- EFP-client/src/App.jsx
+++ EFP-client/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CommonCard from './components/Dashboard/CommonCard'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <CommonCard></CommonCard>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
