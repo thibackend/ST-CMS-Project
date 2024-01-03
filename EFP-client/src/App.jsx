15cd354b6cc8870f1bc005db01f2f20dd7c9c035
Tuyen Error <caotuyenks1605@gmail.com>
2024-01-03 23:07:45 +0700

    interface:Create Dashboard with InfoCard

--- EFP-client/src/App.jsx
+++ EFP-client/src/App.jsx
@@ -9,33 +9,33 @@ import Login from './pages/Auth/Login';
 const { Header, Content } = Layout;
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
+
   const handleLogin = () => {
 
   }
   return (
     <Router>
-      {isLoggedIn? (
-      <Layout style={{ minHeight: '100vh', maxWidth: '100%' }}>
-        <Navbar />
-        <Layout>
-          <Header style={{ background: '#006df0', textAlign:'end'}}>
-            <AvatarComponent />
-          </Header>
-          <Content>
-            <Routes>
-              {routes.Routes.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.element}
-              />
-            ))}
-            </Routes>
-          </Content>
+      {isLoggedIn ? (
+        <Layout style={{ minHeight: '100vh', maxWidth: '100%' }}>
+          <Navbar />
+          <Layout>
+            <Header style={{ background: '#006df0', textAlign: 'end' }}>
+              <AvatarComponent />
+            </Header>
+            <Content>
+              <Routes>
+                {routes.Routes.map((route) => (
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    element={route.element}
+                  />
+                ))}
+              </Routes>
+            </Content>
+          </Layout>
         </Layout>
-      </Layout>
       ) : (
         <Login />
       )}
