2c6d0b664e9eccdb813ca482f92fa13a7cd9eec7
HoVanDi <hovandideveloper@gmail.com>
2024-01-09 23:18:31 +0700

    fix: update code show

--- EFP-client/src/components/SearchAddTab/index.jsx
+++ EFP-client/src/components/SearchAddTab/index.jsx
@@ -1,23 +1,67 @@
 import React from 'react';
-import { Row, Col, Input } from 'antd';
+import { Row, Col, Input, Button } from "antd";
 import { PlusCircleOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
 const SearchAddTab = ({ onSearch, toAddLink }) => {
   return (
-    <Row align="middle" gutter={[16, 0]} style={{background:'#dae1f3',padding: 12, borderRadius: '7px', margin: '0 0 15px'}}>
-      <Col span={7}>
-        <Input.Search
-        size='large'
-          placeholder="Search"
-          onSearch={onSearch}
-          enterButton
+    <Row gutter={[8, 8]} className="mt-6 ml-2 !important">
+      <Col xs={24} sm={24} md={6}>
+        <Input
+          style={{
+            padding: "8px",
+          }}
+          placeholder="Nhập Name"
+          allowClear
+          onChange={(e) => {
+            if (e.type === "click") {
+              handleClear();
+            }
+            setFilters((prevFilters) => ({
+              ...prevFilters,
+              searchByName: e.target.value,
+            }));
+          }}
         />
       </Col>
-      <Col span={16}></Col>
+      <Col xs={24} sm={24} md={6}>
+        <Input
+          style={{
+            padding: "8px",
+          }}
+          placeholder="Email"
+          allowClear
+          onChange={(e) => {
+            if (e.type === "click") {
+              handleClear();
+            }
+            setFilters((prevFilters) => ({
+              ...prevFilters,
+              searchByEmail: e.target.value,
+            }));
+          }}
+        />
+      </Col>
+
+      <Col xs={24} sm={24} md={12} lg={6}>
+        <Row justify="space-between">
+          <Button
+            style={{
+              height: "40px",
+            }}
+            type="primary"
+            onClick={() => handleSearch()}
+          >
+            <span> Search</span>
+          </Button>
+        </Row>
+      </Col>
+
+      <Col span={5}></Col>
+
       <Col span={1}>
         <Link to={toAddLink}>
-          <PlusCircleOutlined style={{ fontSize: '35px' }} />
+          <PlusCircleOutlined style={{ fontSize: "35px" }} />
         </Link>
       </Col>
     </Row>
--- EFP-client/src/pages/Employee/ShowEmployee/index.jsx
+++ EFP-client/src/pages/Employee/ShowEmployee/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-import { Button, Table, Popconfirm, message, Space, Dropdown } from "antd";
+import {
+  Button,
+  Table,
+  Popconfirm,
+  message,
+  Space,
+  Dropdown,
+  Tag,
+  Row,
+  Col,
+} from "antd";
 import {
   EditOutlined,
   DeleteOutlined,
   SearchOutlined,
+  PlusOutlined,
+  PlusCircleOutlined,
 } from "@ant-design/icons";
 import Highlighter from "react-highlight-words";
 import { Link } from "react-router-dom";
@@ -16,6 +28,7 @@ const ShowTable = () => {
   const [searchText, setSearchText] = useState("");
   const [searchedColumn, setSearchedColumn] = useState("");
   const searchInput = useRef(null);
+
   const handleSearch = (selectedKeys, confirm, dataIndex) => {
     confirm();
     setSearchText(selectedKeys[0]);
@@ -26,21 +39,22 @@ const ShowTable = () => {
     setSearchText("");
   };
 
-    const [employee, setEmployee] = useState([]);
+  const [employee, setEmployee] = useState([]);
 
-    useEffect(() => {
-      api.get("/employee").then((res) => setEmployee(res.data));
-    }, [employee]);
+  useEffect(() => {
+    api.get("/employee").then((res) => setEmployee(res.data));
+  }, [employee]);
 
-    const handleDelete = async (employeeId) => {
-      try {
-        await api.delete(`/employee/${employeeId}`);
-        message.success("Deleted successfully");
-      } catch (error) {
-        console.error("Error deleting project:", error);
-      }
-    };
+  const handleDelete = async (employeeId) => {
+    try {
+      await api.delete(`/employee/${employeeId}`);
+      message.success("Deleted successfully");
+    } catch (error) {
+      console.error("Error deleting project:", error);
+    }
+  };
 
+  //search
   const getColumnSearchProps = (dataIndex) => ({
     filterDropdown: ({
       setSelectedKeys,
@@ -131,14 +145,34 @@ const ShowTable = () => {
       ),
   });
 
-   const items = employee.map((record) => ({
-     key: record.id,
-     label: (
-       <p style={{ cursor: "auto" }}>
-         {Array.isArray(record.tech) ? record.tech.join(", ") : record.tech}
-       </p>
-     ),
-   }));
+  const items = employee.map((record) => {
+    const techCount = Array.isArray(record.tech) ? record.tech.length : 0;
+
+    return {
+      key: record.id,
+      label: (
+        <>
+          {Array.isArray(record.tech) &&
+            record.tech.slice(1).map((tech, index) => (
+              <Tag
+                key={`${record.id}_${index}`}
+                color={tech.length > 5 ? "#1677ff" : "#1677ff"}
+                style={{
+                  cursor: "auto",
+                  backgroundColor: "#1677ff",
+                  color: "white",
+                  marginRight: "5px",
+                  fontSize: "16px",
+                  padding: "8px",
+                }}
+              >
+                {tech}
+              </Tag>
+            ))}
+        </>
+      ),
+    };
+  });
 
   const columns = [
     {
@@ -159,15 +193,47 @@ const ShowTable = () => {
       title: "Skill",
       ellipsis: true,
       render: (text, record) => (
-        <Dropdown
-          menu={{
-            items: items.filter((item) => item.key === record.id),
+        <div
+          className="skill"
+          style={{
+            display: "flex",
+            alignItems: "center",
           }}
-          placement="bottomLeft"
-          arrow
         >
-          <div>{record.tech[0]}</div>
-        </Dropdown>
+          <div
+            style={{
+              backgroundColor: "#1677ff",
+              borderRadius: "10px",
+              cursor: "auto",
+              color: "white",
+              fontSize: "16px",
+              padding: "8px",
+            }}
+          >
+            {record.tech[0]}
+          </div>
+
+          <Tag>
+            <Dropdown
+              menu={{
+                items: items.filter((item) => item.key === record.id),
+              }}
+              placement="topLeft"
+              arrow
+            >
+              <div
+                style={{
+                  padding: "8px 9px",
+                  backgroundColor: "aliceblue",
+                  borderRadius: "50%",
+                  border: "1px solid #1677ff",
+                }}
+              >
+                +{record.tech.length - 1 > 0 ? record.tech.length - 1 : ""}
+              </div>
+            </Dropdown>
+          </Tag>
+        </div>
       ),
     },
 
@@ -212,11 +278,14 @@ const ShowTable = () => {
     },
   ];
 
-
-
   return (
     <>
+     
+
       <Table
+        style={{
+          marginTop: "15px",
+        }}
         className="custom-table"
         columns={columns}
         dataSource={employee}
