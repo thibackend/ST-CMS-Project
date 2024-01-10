61502832dc0e57ee845c219230c1132d41a6d09b
unknown <admin@pnvl105.pn.com.vn>
2024-01-11 00:51:23 +0700

    feat: show project and show employee done

--- EFP-client/src/components/SearchAddTab/index.jsx
+++ EFP-client/src/components/SearchAddTab/index.jsx
@@ -150,7 +150,7 @@ export default function Search({
                   icon={<PlusOutlined />}
                   size={"large"}
                 >
-                  New project
+                  New employee
                 </Button>
               </Link>
             </div>
