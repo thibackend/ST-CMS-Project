4ead7a3ecdb7069edc407ede557c88dbbf84a5c8
Tuyen Error <caotuyenks1605@gmail.com>
2024-01-11 10:52:51 +0700

    feat: fix function active/inactive

--- EFP-client/src/pages/Dashboard/index.jsx
+++ EFP-client/src/pages/Dashboard/index.jsx
@@ -1,13 +1,11 @@
-import React ,{useState,useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 import CardComponent from './Cards';
 import { Row, Col } from 'antd';
-import { TeamOutlined, ProjectOutlined, UserOutlined, FileDoneOutlined  } from '@ant-design/icons';
-import {  EmChartComponent, ProChartComponent } from './Charts';
-import { useTranslation } from 'react-i18next';
+import { TeamOutlined, ProjectOutlined, UserOutlined, FileDoneOutlined } from '@ant-design/icons';
+import { EmChartComponent, ProChartComponent } from './Charts';
 import api from '../../services/API_REQ';
 
 const Dashboard = () => {
-    const {t} = useTranslation();
     const [employee, setEmployee] = useState(null);
     const [activeEmployees, setActiveEmployees] = useState(null);
     const [inActiveEmployees, setInActiveEmployees] = useState(null);
@@ -15,8 +13,10 @@ const Dashboard = () => {
     const [projectRuning, setProjectRunning] = useState(null);
     const [projectDone, setProjectDone] = useState(null);
     const [managers, setManagers] = useState(0);
+
     const handleSetProjectRunning = (num) => setProjectRunning(num);
     const handleSetProjectDone = (num) => setProjectDone(num);
+
     const fetchEmployee = async () => {
         await api.get('employee').then(res => {
             if (res.data.length > 0) {
@@ -24,6 +24,7 @@ const Dashboard = () => {
             }
         })
     }
+
     const fetchProject = async () => {
         await api.get('project').then(res => {
             if (res.data.length) {
@@ -35,6 +36,7 @@ const Dashboard = () => {
         await api.get('employee/managers').then(res => {
             if (res) {
                 setManagers(res);
+
             }
         })
     }
@@ -43,10 +45,12 @@ const Dashboard = () => {
             const activeEmployeesData = employee.filter(employee => employee.status === 'active');
             const inActiveEmployeesData = employee.filter(employee => employee.status === 'inactive');
             if (activeEmployeesData) {
+                console.log("ActiveEmployeesData: ", activeEmployeesData);
                 setActiveEmployees(activeEmployeesData);
             }
 
             if (inActiveEmployeesData) {
+                console.log("inActiveEmployeesData: ", inActiveEmployeesData);
                 setInActiveEmployees(inActiveEmployeesData);
             }
         }
@@ -60,24 +64,24 @@ const Dashboard = () => {
     }, []);
     return (
         <>
-            <Row gutter={[10, 10]} style={{ marginBottom:40 }}>
+            <Row gutter={[10, 10]} style={{ minHeight: '35vh' }}>
                 <Col xs={24} sm={12} md={6}>
                     <CardComponent title="All employees" count={employee ? employee.length : 0} icon={<TeamOutlined />} className="employee-card" />
                 </Col>
                 <Col xs={24} sm={12} md={6}>
-                    <CardComponent title={t('dashboard.all_managers')} count={managers ? managers.length : 0} icon={<UserOutlined />} className="manager-card" />
+                    <CardComponent title="All managers" count={managers ? managers.length : 0} icon={<UserOutlined />} className="manager-card" />
                 </Col>
                 <Col xs={24} sm={12} md={6}>
-                    <CardComponent title={t('dashboard.all_running_projects')} count={projectRuning ? projectRuning : 0} icon={<ProjectOutlined />} className="running-project-card" />
+                    <CardComponent title="All running projects" count={projectRuning ? projectRuning : 0} icon={<ProjectOutlined />} className="running-project-card" />
                 </Col>
                 <Col xs={24} sm={12} md={6}>
-                    <CardComponent  nent title={t('dashboard.all_done_projects')} count={projectDone ? projectDone : 0} icon={<FileDoneOutlined />} className="done-project-card" />
+                    <CardComponent title="All done projects" count={projectDone ? projectDone : 0} icon={<FileDoneOutlined />} className="done-project-card" />
                 </Col>
             </Row>
             {console.log("render: ", activeEmployees && activeEmployees.length)}
             <Row gutter={[10, 10]}>
-                <Col span={13} style={{ background: '#ecf0f4', padding: '10px' }}>
-                    <EmChartComponent />
+                <Col xs={24} sm={24} md={13} style={{ background: '#ecf0f4', padding: '10px' }}>
+                    <EmChartComponent activeEmployees={activeEmployees && activeEmployees.length} inActiveEmployees={inActiveEmployees && inActiveEmployees.length} />
                 </Col>
                 <Col xs={0} sm={0} md={1}></Col>
                 <Col xs={24} sm={24} md={10} style={{ background: '#ecf0f4', padding: '10px' }}>
